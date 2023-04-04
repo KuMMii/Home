01c2b814b356d78ef1de09423cf23c9f2aa2da17
KuMMii <4.prgmng@gmail.com>
2023-04-04 22:30:58 +0900

    commit

---
+++ Study_Home/src/mar_2023/ServerExample.java
@@ -0,0 +1,72 @@
+package mar_2023;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerExample {
+	private static ServerSocket serverSocket;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("-----------------------------");
+		System.out.println("Type q or Q and Enter to Quit the server");
+		System.out.println("-----------------------------");
+		
+		//TCP Server Start
+		startServer();
+		
+		//Keyboard Input
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			String key = scanner.nextLine();
+			if(key.toLowerCase().equals("q")) {
+				break;
+			}
+		}
+		scanner.close();
+		
+		//TCP Server close
+		stopServer();
+	}
+	public static void startServer() {
+		//�۾� ������ ����
+		Thread thread = new Thread() {
+			public void run() {
+				try {
+					//ServerSocket generate &bind Port
+					serverSocket= new ServerSocket(50001);
+					System.out.println("[Server] Start");
+					
+					while(true) {
+						System.out.println("\n[Server] wating for connection\n");
+						//���� ����
+						Socket socket = serverSocket.accept();
+						//Connected Client Info
+						InetSocketAddress isa = (InetSocketAddress)socket.getRemoteSocketAddress();
+						String clientIp = isa.getHostString();
+						System.out.println("[Server] "+clientIp+" allowing Connection");
+						//Disconnecting
+						socket.close();
+						System.out.println("[Server] "+clientIp+" disconnected");
+					}
+				} catch (IOException e) {
+					System.out.println("[Server]"+e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		};
+		//Thread Start
+		thread.start();
+	}
+	
+	public static void stopServer() {
+		//Closing ServerSocket and Unbinding Port
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+		}
+	}
+}
