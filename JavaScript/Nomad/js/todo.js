3b43ee987c2ab4cf01436ad56265f8fa9de2d19f
KuMMii <128502524+KuMMii@users.noreply.github.com>
2023-04-12 22:49:00 +0900

    Add files via upload
    https://iwannabealivejobs.netlify.app/
    My first webpage!
    css source from : https://codesandbox.io/s/0618momentum-8d3o3?file=/index.html

--- JavaScript/Nomad/js/todo.js
+++ JavaScript/Nomad/js/todo.js
@@ -5,11 +5,13 @@ const toDoList=document.getElementById("todo-list");
 
 const TODOS_KEY="todos";
 
-const toDos= [];
+let toDos= [];
 
 function deleteToDo(event){
     const li=event.target.parentElement;
     li.remove();
+    toDos=toDos.filter((toDo)=>toDo.id !==parseInt(li.id));
+    saveToDos();
 }
 
 function saveToDos(){
@@ -20,14 +22,16 @@ function saveToDos(){
 
 function paintToDo(newTodo){
 const li = document.createElement('li');
+li.id=newTodo.id;
 const span = document.createElement('span');
-span.innerText=newTodo;
+span.innerText=newTodo.text;
 const button = document.createElement("button");
 button.innerText="❌";
 button.addEventListener("click",deleteToDo)
 
 li.appendChild(span);
 li.appendChild(button);
+
 toDoList.appendChild(li);
 
 }
@@ -36,8 +40,12 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo=toDoInput.value;
     toDoInput.value="";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj={
+        text:newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -52,5 +60,6 @@ const savedToDos=localStorage.getItem(TODOS_KEY);
 
 if(savedToDos!== null){
     const parsedToDos=JSON.parse(savedToDos);
-    parsedToDos.forEach((item)=>console.log("hello",item));
+    toDos=parsedToDos;
+    parsedToDos.forEach(paintToDo);
 }
