bc56204766ddd090a754b6900534b31a7573157c
KuMMii <4.prgmng@gmail.com>
2023-04-19 23:24:56 +0900

    loops-for,while,do while

---
+++ Java/src/apr_2023/Loop_03While.java
@@ -0,0 +1,12 @@
+package apr_2023;
+
+public class Loop_03While {
+	public static void main(String[] args) {
+		WhileNumberPlayer player = new WhileNumberPlayer(30);
+		
+		player.printSquaresUptoLimit();
+		player.printCubesUptoLimit();
+		
+	}
+
+}
---
+++ Java/src/apr_2023/Loop_05DoWhile.java
@@ -0,0 +1,19 @@
+package apr_2023;
+
+import java.util.Scanner;
+
+public class Loop_05DoWhile {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number=-1;
+		
+		do {
+			if(number != -1)	System.out.println("Cube is "+(number*number*number));
+			
+			System.out.print("Enter a number: ");
+			number=sc.nextInt();
+		}while(number>=0);
+		System.out.println("Thank You! Have Fun!");
+	}
+}
