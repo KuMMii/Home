183074a3a9d6e550dfe29449e838b1c8c46292d9
KuMMii <4.prgmng@gmail.com>
2023-04-20 23:28:19 +0900

    String, Wrapper, Date

---
+++ Java/src/apr_2023/Date_01.java
@@ -0,0 +1,16 @@
+package apr_2023;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Date_01 {
+
+	public static void main(String[] args) {
+		LocalDate now = LocalDate.now();
+		LocalDateTime now2 = LocalDateTime.now();
+		LocalTime now3=LocalTime.now();
+		
+		System.out.println(now);
+	}
+}
