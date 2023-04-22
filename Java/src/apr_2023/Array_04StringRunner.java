658cde346f4d99d1c7f788ac80abbfaad8538a2e
KuMMii <4.prgmng@gmail.com>
2023-04-23 00:00:00 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ Java/src/apr_2023/Array_04StringRunner.java
@@ -0,0 +1,22 @@
+package apr_2023;
+
+public class Array_04StringRunner {
+ public static void main(String[] args) {
+	String [] week = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+
+	String longDay="";
+	for(String day:week) {
+		if(longDay.length()<day.length())
+			longDay=day;
+	}
+	 System.out.println(longDay);
+	 
+	 
+	 for(int i=(week.length-1);i>=0;i--) {
+		 System.out.println(week[i]);
+	 }
+	 
+	 
+}
+ 
+}
