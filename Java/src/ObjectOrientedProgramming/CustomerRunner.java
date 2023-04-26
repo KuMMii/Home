b211861dbdbc86f2def51247d5bbfdd6650387cc
KuMMii <4.prgmng@gmail.com>
2023-04-26 23:32:41 +0900

    object oriented

---
+++ Java/src/ObjectOrientedProgramming/CustomerRunner.java
@@ -0,0 +1,17 @@
+package ObjectOrientedProgramming;
+
+public class CustomerRunner {
+
+	public static void main(String[] args) {
+		
+		Address homeAddress= new Address("line 1", "Hyderabad", "49932");
+		Customer customer = new Customer("Ranga", homeAddress);
+		System.out.println(customer);
+
+		Address workAddress = new Address("line 1 for work", "Hyderabad", "23313");
+		customer.setWorkAddress(workAddress);
+		
+		System.out.println(customer);
+	}
+
+}
